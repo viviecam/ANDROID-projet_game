9b54bec75e1d99fefc27330b73a6e53ff5398354
viviecam <camille.vivier@hotmail.fr>
2018-03-12 16:23:24 +0100

    UPDATE Generate plateform coordonate loop - STILL NOT FUNCTIONAL

--- app/src/main/java/com/example/bozonpee/canvasaccelerometer/Main.java
+++ app/src/main/java/com/example/bozonpee/canvasaccelerometer/Main.java
@@ -20,6 +20,8 @@ import android.util.Log;
 import android.view.Display;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -41,8 +43,8 @@ public class Main extends AppCompatActivity implements SensorEventListener {
     private float circleX; //Position sur l'axe X
     private float circleY; //Position sur l'axe Y
 
-    //Tableau de Plateformes
-    private Plateform plateforms[];
+    //Liste de Plateformes
+    private List<Plateform> plateforms;
 
     //Paramètres à prendre en compte pour la génération aléatoire des plateformes
     private int minX = 0;
@@ -167,7 +169,8 @@ public class Main extends AppCompatActivity implements SensorEventListener {
         screenHeight = screenSize.y;
 
         //On appelle la fonction pour générer les plateformes, en lui passant les paramètres nécéssaires
-        generatePlateformsPositions(10, 0, 0, screenWidth, screenHeight);
+        //Et on stocke le retour de cette fonction, dans notre liste de plateformes
+        plateforms = generatePlateformsPositions(10, 0, 0, screenWidth, screenHeight);
 
         //On place le personnage (point) au center de l'écran
         circleX = screenWidth / 2 - circleRadius;
@@ -195,67 +198,67 @@ public class Main extends AppCompatActivity implements SensorEventListener {
     }
 
     //Fonction pour générer les coordonnées de départ de dessin des plateformes
-    public void generatePlateformsPositions(int nbPlateforms, int minX, int minY, int maxX, int maxY) {
+    public List<Plateform> generatePlateformsPositions(int nbPlateforms, int minX, int minY, int maxX, int maxY) {
         int i = 0;
+        //Nouvelle liste dans la zone ou l'on veut créer les plateformes
+        List<Plateform> listeProvisoire = new ArrayList<>();
 
         while (i <= nbPlateforms-1) {
             boolean invalidCoordonate = false;
 
-            //On vérifie que la case du tableau sur laquelle on est, est bien vide
-            if (plateforms[i] == null) {
+            //On génère un nombre aléatoire entre les valeurs des coordonées (largeur et hauteur) de la zone
+            //ou l'on veut rajouter des plateformes
+            //Moins la longueur (100) et épaisseur(40) d'une plateforme
+            //Pour eviter qu'une plateforme soit générée au bord de l'écran, et donc ne soit pas visible
+            Random randomCoordonate = new Random();
+            //Par défaut, nextInt retourne un nombre aléatoire entre 0 et le nombre qu'on lui passe en paramètre
+            //Avec 0 inclus, et le nombre en paramètre exclus
+            //Donc, pour gérérer un nombre entre a et b, les deux inclus et a différent de 0
+            int randomX = randomCoordonate.nextInt((maxX - 99) - minX + 1) + minX;
+            int randomY = randomCoordonate.nextInt((maxY - 39) - minY + 1) + minY;
 
-                //On génère un nombre aléatoire entre les valeurs des coordonées (largeur et hauteur) de la zone
-                //ou l'on veut rajouter des plateformes
-                //Moins la longueur (100) et épaisseur(40) d'une plateforme
-                //Pour eviter qu'une plateforme soit générée au bord de l'écran, et donc ne soit pas visible
-                Random randomCoordonate = new Random();
-                //Par défaut, nextInt retourne un nombre aléatoire entre 0 et le nombre qu'on lui passe en paramètre
-                //Avec 0 inclus, et le nombre en paramètre exclus
-                //Donc, pour gérérer un nombre entre a et b, les deux inclus et a différent de 0
-                int randomX = randomCoordonate.nextInt((maxX - 99) - minX + 1) + minX;
-                int randomY = randomCoordonate.nextInt((maxY - 39) - minY + 1) + minY;
-
-                //On parcours le tableau plateforms
-                for(int j=0; j<i; j++){
-
-                    // On verifie que la case courante est non nulle
-                    // ET que ce n'est pas la même case qu'en i ????
-                    if (plateforms[j] != null && j != i) {
-                        //On vérifie que les coordonnées générées ne soient pas communs à une plateforme déjà existante
-                        // (-100), (+100), (-40), (+40) sont la pour être sur que les plateformes ne se chevauchent pas
-                        // Reminder : le x et le y de chaque plateforme correspond à son point de départ en haut à gauche
-                        if (randomX >= plateforms[j].getPlateformX()-100 && randomX <= plateforms[j].getPlateformX()+100
-                                && randomY >= plateforms[j].getPlateformY()-40 && randomX <= plateforms[j].getPlateformY()+40 ) {
-                            invalidCoordonate = true;
-                            //On a trouvé une plateforme qui est déjà au même endroit, donc on sort de la boucle for
-                            break;
-                        }
-                        // Si on ne rentre pas dans la condition if ci-dessus
-                        j++;
-                        //Et on vérifie avec la plateforme suivante dans le tableau
-                       }
+            //Si la liste provisoire n'est pas vide
+            if (listeProvisoire.isEmpty() == false) {
+                //On parcours la liste provisoire
+                //Pour tous les élements de liste
+                for (int j = 0; j < listeProvisoire.size(); j++) {
+                    //On récupère la plateforme courante dans la liste (comme si listeProvisoire[j] sur un tableau)
+                    Plateform item = listeProvisoire.get(j);
+                    //On vérifie que les coordonnées générées ne soient pas communs à une plateforme déjà existante
+                    // (-100), (+100), (-40), (+40) sont la pour être sur que les plateformes ne se chevauchent pas
+                    // Reminder : le x et le y de chaque plateforme correspond à son point de départ en haut à gauche
+                    if (randomX >= item.getPlateformX() - 100 && randomX <= item.getPlateformX() + 100
+                            && randomY >= item.getPlateformY() - 40 && randomX <= item.getPlateformY() + 40) {
+                        invalidCoordonate = true;
+                        //On a trouvé une plateforme qui est déjà au même endroit, donc on sort de la boucle for
+                        break;
                     }
+                }
 
                 //Si on a pas trouvé de point de contact avec une autre plateform
                 if (invalidCoordonate = false) {
                     //Alors on ajoute la nouvelle plateforme avec les coordonnées générés, au tableau plateforms
-                    plateforms[i].setPlateformX(randomX);
-                    plateforms[i].setPlateformY(randomY);
-                    plateforms[i] = new Plateform(randomX, randomY);
+                /*plateforms[i].setPlateformX(randomX);
+                plateforms[i].setPlateformY(randomY);*/
+                    listeProvisoire.add(new Plateform(randomX, randomY));
                     // Et on passe à la case suivante du tableau
                     i++;
                 }
                 //Sinon, on reste dans la même case et on va générer de nouveaux coordonnées
 
             }
-            //Si elle contient déjà une plateforme, on passe à la case suivante
+
+            // Si la liste provisoire est vide
             else {
+                // On peut ajouter directement les valeurs générées
+                listeProvisoire.add(new Plateform(randomX, randomY));
+                // Et on peut passer à la génération de la prochaine plateforme
                 i++;
             }
 
-
         }
 
+        return listeProvisoire;
 
     }
 
